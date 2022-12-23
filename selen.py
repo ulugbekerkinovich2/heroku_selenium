81864a5768cb09b5d865b6a14d03ab687cabf645
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-23 21:46:15 +0500

    'sds'

--- selen.py
+++ selen.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,10 +10,11 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-driver = webdriver.Chrome(executable_path="C:/Users/ulugbek/PycharmProjects/deploy_heroku_selenium/chromedriver")
+# driver = webdriver.Chrome(executable_path="C:/Users/ulugbek/PycharmProjects/deploy_heroku_selenium/chromedriver")
 # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 print('ornatildi')
 driver.get("https://www.lsgeotar.ru/pages/abc-pharma_tn.html")
+time.sleep(15)
 links = driver.find_elements(By.TAG_NAME, 'a')
 # print("Opened database successfully")
 list_links = []
@@ -32,3 +34,6 @@ for link in links:
         if a % 2 == 0:
             list_links.append(link_l)
 print(list_links)
+
+time.sleep(10)
+driver.close()
